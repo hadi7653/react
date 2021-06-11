1fdbfef1e7102552b2360c2f674a1ed145831212
ABDUL HADI BP <safu7653@gmail.com>
2021-06-11 17:48:05 +0530

    added

---
+++ src/Constants/Constants.js
@@ -0,0 +1,2 @@
+export const baseUrl = 'https://api.themoviedb.org/3';
+export const Api_Key = '53f959feb2d1d5f32962c070824d40dd';
---
+++ src/componanats/Banner/Banner.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import {Api_Key} from '../../Constants/Constants'
+import {useEffect} from 'react'
+import axios from  '../../axios'
+import './Banner.css'
+
+function Banner() {
+    useEffect(() => {
+        axios.get(`trending/all/week?api_key=${Api_Key}&language=en-US`).then((response)=>{
+            console.log(response.data)
+        })
+        
+    })
+    return (
+        <div className="banner">
+            <div className='content'>
+                <h1 className="title" >Movie Name</h1>
+                <div className="banner-button">
+                    <button className="button">Play</button>
+                    <button className="button">My List</button>
+                </div>
+                <h1 className='discription'>In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content. Lorem ipsum may be used as a placeholder before final copy is available.</h1>
+            </div>
+            <div className="fade"></div>
+        
+        </div>
+    )
+}
+
+export default Banner
